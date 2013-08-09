457f2aadf19f5f9c8a80d2c214429c34cf35d56f
René Kistl <rene@kistl.at>
2013-08-09 23:30:14 +0200

    Add gobrpc.

---
+++ examples/server-gobrpc.go
@@ -0,0 +1,66 @@
+// Copyright 2013 René Kistl. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"github.com/pcdummy/bidirpc"
+	"github.com/pcdummy/bidirpc/gobrpc"
+	"log"
+	"net"
+)
+
+type Arith struct {
+	count int
+	p     *bidirpc.Protocol
+}
+
+type EchoArgs struct {
+	EchoMe string `bson:"echoMe"`
+}
+
+type EchoReply struct {
+	Result string
+}
+
+// The protocol excutes this method when it finds it.
+// this allows the Server to do t.p.Call() / t.p.Go() on the client.
+func (t *Arith) SetProtocol(p *bidirpc.Protocol) {
+	t.p = p
+}
+
+func (t *Arith) Echo(args *EchoArgs, reply *EchoReply) (err error) {
+	fmt.Println("Echo from client:", args.EchoMe)
+
+	// Run a call on the client
+	var nreply EchoReply
+	err = t.p.Call("Arith.Echo", &EchoArgs{EchoMe: "Hey client how are you?"}, &nreply)
+
+	reply.Result = args.EchoMe
+	return err
+}
+
+func main() {
+	l, err := net.Listen("tcp", ":9990")
+	if err != nil {
+		log.Fatal("listen error:", err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal("accept error:", err)
+		}
+
+		// Every connection needs a codec, a new Protocol
+		// and the methods needs to be Registered at every session.
+		// The methods need to be goroutine save and they
+		// are unique per session.
+		c := gobrpc.NewCodec(conn)
+		s := bidirpc.NewProtocol(c)
+		s.Register(new(Arith))
+		go s.Serve()
+	}
+}
---
+++ gobrpc/gobrpc.go
@@ -0,0 +1,65 @@
+// Copyright 2013 René Kistl. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gobrpc
+
+import (
+	"bufio"
+	"encoding/gob"
+	"github.com/pcdummy/bidirpc"
+	"io"
+)
+
+func NewClient(conn io.ReadWriteCloser) (c *bidirpc.Protocol) {
+	cc := NewCodec(conn)
+	c = bidirpc.NewClientWithCodec(cc)
+	return
+}
+
+func NewCodec(conn io.ReadWriteCloser) (cc bidirpc.Codec) {
+	wBuf := bufio.NewWriter(conn)
+	cc = &codec{
+		conn: conn,
+		dec:  gob.NewDecoder(conn),
+		enc:  gob.NewEncoder(wBuf),
+		wBuf: wBuf,
+	}
+	return
+}
+
+type codec struct {
+	conn io.ReadWriteCloser
+	dec  *gob.Decoder
+	enc  *gob.Encoder
+	wBuf *bufio.Writer
+}
+
+func (c *codec) Write(rs *bidirpc.RepReq, v interface{}) (err error) {
+	if err = c.enc.Encode(rs); err != nil {
+		return
+	}
+
+	if err = c.enc.Encode(v); err != nil {
+		return
+	}
+
+	c.wBuf.Flush()
+
+	return
+}
+
+func (c *codec) ReadHeader(res *bidirpc.RepReq) (err error) {
+	err = c.dec.Decode(res)
+	return
+}
+
+func (c *codec) ReadBody(v interface{}) (err error) {
+	err = c.dec.Decode(v)
+	return
+}
+
+func (c *codec) Close() (err error) {
+	err = c.conn.Close()
+	return
+}
